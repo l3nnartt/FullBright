14053dc6c5c8ae3d1ddee671502857583568633b
Lennart Lösche <loesche.lennart@gmail.com>
2022-10-05 09:13:32 +0200

    bump version 1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,7 +65,7 @@ addon {
         iconUrl("https://dl.lennartloesche.de/fullbright/logo.png", project(":core"))
         author("L3nnart_")
         description("Allows you to change the brightness of the game to full bright")
-        version("1.2")
+        version("1.3")
         minecraftVersion("1.8<1.18")
     }
     
